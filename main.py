1fe1558291da57150dd7964e3537327a13fa8b52
Grigoriev.Vladimir <Hronos.Black@gmail.com>
2024-10-28 16:55:06 +0500

    Тестовый main. Исчезнет из итогового проекта

---
+++ main.py
@@ -0,0 +1,15 @@
+from objects import Genre
+from core import AT
+
+
+def print_item_is_list(list, pref):
+    for item in list:
+        print(f"{pref}{str(item)}")
+        if len(item.Childs) > 0:
+            print_item_is_list(item.Childs, (pref + "\t"))
+
+
+if __name__ == "__main__":
+    at = AT()
+    login_ans = at.Login("Hronos.Black@gmail.com", "Rtu87qw1")
+    print_item_is_list(at.AllGenres, "")
