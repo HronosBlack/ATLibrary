c4674d59d84bc2f2d466086c6565010f5ed00605
Grigoriev.Vladimir <Hronos.Black@gmail.com>
2024-10-28 16:54:43 +0500

    Скелет класса ядра работы с онлайн библиотекой (авторизация, запросы и др)

---
+++ core.py
@@ -0,0 +1,106 @@
+import requests
+
+from objects import Genre
+
+
+class AT:
+    
+    def __init__(self) -> None:
+        self.__API: str = "https://api.author.today"
+        self.__WEB_API: str = "https://author.today"
+        self.__TOKEN: str = "Bearer guest"
+        self.__HEADERS: dict = {
+            "authorization": self.__TOKEN,
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
+            "x-requested-with": "XMLHttpRequest"
+        }
+        self.__GENRES: list[Genre] = []
+    
+    def Login(self, user_login: str, user_password: str) -> dict:
+        data = {
+            "login": user_login,
+            "password": user_password
+        }
+        response = requests.post(
+            f"{self.__API}/v1/account/login-by-password",
+            json=data,
+            headers=self.__HEADERS,
+            verify=False
+        ).json()
+        
+        if "token" in response:
+            self.__TOKEN = response["token"]
+            self.__HEADERS["authorization"] = f"Bearer {self.__TOKEN}"
+            self.__USER_ID = self.GetAccountInfo()["id"]
+        
+        return response
+    
+    def LoginWithToken(self, token: str) -> dict:
+        self.__TOKEN = token
+        self.__HEADERS["authorization"] = f"Bearer {self.__TOKEN}"
+        response = self.GetAccountInfo()
+        if "id" in response:
+            self.__USER_ID = response["id"]
+        return response
+    
+    def GetAccountInfo(self) -> dict:
+        return requests.get(
+            f"{self.__API}/v1/account/current-user",
+            headers=self.__HEADERS,
+            verify=False
+        ).json()
+        
+    def RefreshToken(self) -> dict:
+        return requests.post(
+            f"{self.__API}/v1/account/refresh-token",
+            headers=self.__HEADERS,
+            verify=False
+        ).json()
+        
+    def __GetAllGenres__(self) -> dict:
+        genres = requests.get(
+            f"{self.__API}/v1/work/genres",
+            headers=self.__HEADERS,
+            verify=False
+        ).json()
+        
+        for genre in genres:
+            genre_name: str = genre["title"]
+            genre_key: str = genre["code"]
+            genre_id: int = int(genre["id"])
+            
+            parent_genre = None
+            if genre["parentId"] != None:
+                parent_genre = self.__FindGenreInAll(genre["parentId"])
+            
+            if parent_genre != None:
+                parent_genre.Childs.append(Genre(genre_name, genre_key, genre_id))
+            else:
+                self.__GENRES.append(Genre(genre_name, genre_key, genre_id))
+            
+        return genres
+
+    @property
+    def AllGenres(self) -> list[Genre]:
+        if len(self.__GENRES) == 0:
+            self.__GetAllGenres__()
+        return self.__GENRES
+    
+    def __FindGenreInAll(self, genre_id: str | int, all_genre: list[Genre] = None) -> Genre | None:
+        if all_genre == None:
+            all_genre = self.__GENRES
+            
+        if type(genre_id) == str:
+            if genre_id.isnumeric():
+                genre_id = int(genre_id)
+        
+        for genre in all_genre:
+            if genre.Id == genre_id:
+                return genre
+            else:
+                if len(genre.Childs) > 0:
+                    child = self.__FindGenreInAll(genre_id, genre.Childs)
+                    if child:
+                        return child
+        
+        return None
